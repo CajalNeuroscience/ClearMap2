557913b6d99e4d055b726a02ba67fa25930aa5fc
Christian Gaetano <cgaetano@cajalneuro.com>
2022-05-31 20:17:11 -0700

    Add metadata

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ from distutils.core import setup
 
 setup(name='ClearMap',
       version='2.0',
+      description='ClearMap is a toolbox for the analysis and registration of volumetric data from cleared tissues.',
+      author='Christoph Kirst',
+      url='https://christophkirst.github.io/ClearMap2Documentation',
       packages=['ClearMap'],
      )
